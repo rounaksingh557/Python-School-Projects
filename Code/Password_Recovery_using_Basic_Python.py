63c2eab1055df39f1807701c7ab6aba2d5bd7116
Rounak Singh <rounaksingh9431738764@gmail.com>
2022-12-11 20:55:52 +0530

    Intial Commit

---
+++ Code/Password_Recovery_using_Basic_Python.py
@@ -0,0 +1,70 @@
+password = "Davian"
+username = "Sanchit"
+
+
+def inputting_username():
+    global ue
+    ue = input("Enter username")
+    if ue == username:
+        inputting_password()
+    else:
+        print("The username is not correct please try again")
+        inputting_username()
+
+
+def inputting_password():
+    pe = input("Enter password")
+    if pe == password:
+        print("welocme", ue)
+    else:
+        print("The password is incorrect")
+        decision()
+
+
+def decision():
+    decision = input(
+        "If you want to retrieve password enter 'A' and if you want to try entering again enter 'B'")
+    print()
+    if decision == 'B':
+        inputting_password()
+    elif decision == "A":
+        retrieve()
+    else:
+        print("Wrong decision")
+
+
+def retrieve():
+    guess = input("Guess your password")
+    if len(guess) != len(password):
+        if len(guess) > len(password):
+            print("it is longer than the registered password")
+        else:
+            print("It is smaller than the registered password")
+        retrieve()
+    else:
+        if guess == password:
+            print("The password is retrieved:", password)
+            exit()
+        else:
+            print("Guessed password is of the length of the registered password")
+            s = ""
+            for i in guess:
+                if i in password:
+                    print(i, " exists in registered password")
+                    s = s+i
+            if len(s) == len(password):
+                if s == password:
+                    print("password retrieved")
+                    exit()
+                else:
+                    print(
+                        "All the characters of password are present in the guessed password. Try resuffling the characters")
+                    print("The first 2 characters of teh password are:",
+                          password[0], password[1])
+            print("Retry guessing password again")
+            print()
+            retrieve()
+
+
+def call():
+    inputting_username()
---
+++ Readme.md
@@ -0,0 +1,23 @@
+# School Python Projects
+
+## <pre>In this repository you will python projects curated by efforts of <mark>Rounak Singh and Sanchit Ark</mark>. </pre>
+
+<br />
+
+### <pre>To Create Activate Virtual Environment, and install dependencies follow this guide 👇</pre>
+```sh
+python -m venv
+source Scripts/activate
+python install requirements.txt
+# Via Git Bash
+```
+
+### <pre>All the code is present in [Code Directory](), please click on any file to view it.</pre>
+
+### <pre>If you have a github account, you may hit period key, (.) on your keyboard to open vscode web editor in your browser.</pre>
+
+### <pre>Visit [SR DAV PUNDAG SCHOOL](http://srdavpundag.org/) also check [Rounak Singh at github](https://github.com/rounaksingh557) </pre>
+
+### <pre>Sanchit's Github Account will be announced soon.</pre>
+
+### <pre><marquee>Credit: Rounak and Sanchit, special Thanks: Mr. Sudhanshu Srivastav Sir.</marquee> </pre>
