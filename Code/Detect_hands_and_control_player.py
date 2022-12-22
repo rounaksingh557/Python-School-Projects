9a3f88eb980e2bb48e8cda1aa718a18caf5d3725
Rounak Singh <rounaksingh9431738764@gmail.com>
2022-12-22 16:19:22 +0530

    Project Started

---
+++ Code/Detect_hands_and_control_player.py
@@ -0,0 +1,24 @@
+import cv2
+import mediapipe as mp
+
+camera = cv2.VideoCapture(0)
+
+
+solutions_hand = mp.solutions.hands
+solutions_draw = mp.solutions.drawing_utils
+
+hands = solutions_hand.Hands(min_detection_confidence=0.8,
+                             min_tracking_confidence=0.5)
+
+while True:
+    success, image = camera.read()
+    image = cv2.flip(image, 1)
+    cv2.imshow("Screen", image)
+
+    results = hands.process(image)
+
+    key = cv2.waitKey(1)
+    if key == 32:
+        break
+
+cv2.destroyAllWindows()
